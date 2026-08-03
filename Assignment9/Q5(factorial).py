8fea4e463db3dd6037f8fa74f76e3ed4ad258a29
Chaitrali Shinde <chaitralishinde984@gmail.com>
2026-08-03 20:41:56 +0530

    Delete Assignment9 directory

--- Assignment9/Q5(factorial).py
+++
@@ -1,9 +0,0 @@
-# WAP to find factorial using recursion 
-
-def factorial(num):
-    if num == 0 or num == 1:
-        return 1
-    return num * factorial(num-1)
-num = int(input("Enter value of num : "))
-res = factorial(num)
-print(f'Factorial of {num} is {res}.')
