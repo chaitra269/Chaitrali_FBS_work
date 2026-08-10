322814cb9e5cb16ed0800b9ce1039f508ecac8a9
Chaitrali Shinde <chaitralishinde984@gmail.com>
2026-08-10 18:10:19 +0530

    Refactor to use list comprehensions for squares and cubes

--- Core Python/Assignments/Assignment10/Q12(squares&cubes).py
+++ Core Python/Assignments/Assignment10/Q12(squares&cubes).py
@@ -1,13 +1,9 @@
 # WAP to create three lists of numbers ,their squares and cubes 
 
-li = [1,2,3,4,5]
-square_li = []
-cube_li = []
-for i in li:
-    ele = i**2
-    square_li.append(ele)
-    ele = i**3
-    cube_li.append(ele)
-print('Original list = ',li)
-print('Square list of original list = ',square_li)
-print('Cube list of original list = ',cube_li)
+numbers = list(range(1, 11))
+squares = [num ** 2 for num in numbers]
+cubes = [num ** 3 for num in numbers]
+
+print("Numbers: ", numbers)
+print("Squares: ", squares)
+print("Cubes:   ", cubes)
