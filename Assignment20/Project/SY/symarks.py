00f6367f340da8a9e1b24331a82ae52642746cb8
Chaitrali Shinde <chaitralishinde984@gmail.com>
2026-09-23 10:21:32 +0530

    Add files via upload

---
+++ Assignment20/Project/SY/symarks.py
@@ -0,0 +1,6 @@
+
+class SYMARKS:
+    def __init__(self, computer_total, maths_total, electronics_total):
+        self.computer_total = computer_total
+        self.maths_total = maths_total
+        self.electronics_total = electronics_total
