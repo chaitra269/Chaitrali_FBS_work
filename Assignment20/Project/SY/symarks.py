1de1b76d84870168cbc71109afae698a04c40331
Chaitrali Shinde <chaitralishinde984@gmail.com>
2026-09-23 10:22:35 +0530

    Delete Assignment20 directory

--- Assignment20/Project/SY/symarks.py
+++
@@ -1,6 +0,0 @@
-
-class SYMARKS:
-    def __init__(self, computer_total, maths_total, electronics_total):
-        self.computer_total = computer_total
-        self.maths_total = maths_total
-        self.electronics_total = electronics_total
--- Assignment20/Project/TY/tymarks.py
+++
@@ -1,5 +0,0 @@
-
-class TYMarks:
-    def __init__(self, theory, practical):
-        self.theory = theory
-        self.practical = practical
